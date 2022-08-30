36e53b6d014f4beea420d4969a89e0ccc043f889
Michaela Healton <mshealton@gmail.com>
2022-08-29 17:59:26 -0700

    Stuck on kidsGame

---
+++ objects.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// 1. countWords
+function countWords(phrase) {
+  const wordCounts = {};
+  for (const word of phrase.split(" ")) {
+    if (wordCounts.hasOwnProperty(word)) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+  return wordCounts;
+}
+
+// 2. getMelonsAtPrice
+function getMelonsAtPrice(price) {
+  const melons = {2.50:['canteloupe', 'honeydew'],
+    2.95:['watermelon'],
+    3.25:['musk', 'crenshaw'],
+    14.25:['christmas'] 
+  };
+  if (!melons.hasOwnProperty(price)) {
+    return;
+  }
+  return melons[price].sort((a,b) => (a - b));
+}
